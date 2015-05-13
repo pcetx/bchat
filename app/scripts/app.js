17d0355a1a5500e07f5a60f3517b11a23e1e139f
pcetx <moulthunl@gmail.com>
2015-05-13 13:53:48 +1000

    Added modal and adding some rooms work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,19 +18,29 @@ var app = angular.module('bchatApp', [
     'ngTouch',
     'firebase',
     'firebase.ref',
-    'firebase.auth'
+    'firebase.auth',
+    'ui.bootstrap',
+    'angular-loading-bar'
 ]);
 
 app.factory('Rooms', ['$firebaseArray','Ref', function($firebaseArray, Ref) {
 
   //var firebaseRef = new Firebase('https://bchat401.firebaseio.com');
-  var all = $firebaseArray(Ref.child('rooms').limitToLast(10));
-  var create = function(name){
+  var all = $firebaseArray(Ref.child('rooms'));
 
+  //Add a new chatroom to the List according to input name
+  var create = function(name){
+    all.$add(name);
   };
+
+  //Remove a room based on name
+  var remove = function(name){
+    all.$remove(name);
+  };
+
   return {
     all: all,
-    create: create 
-
+    create: create, 
+    remove: remove
   }
 }])
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the bchatApp
  */
 angular.module('bchatApp')
-  .controller('MainCtrl', function ($scope, $firebaseArray, Ref, Rooms) {
+  .controller('MainCtrl', function ($scope, $firebaseArray, Ref, Rooms, $modal) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -16,14 +16,33 @@ angular.module('bchatApp')
     ];
 
     $scope.rooms = Rooms.all;
-    //$scope.rooms = $firebaseArray(Ref.child('rooms').limitToLast(10));
+    $scope.animationsEnabled = true;
 
-    $scope.clickModal = function (event) {
-      if (event.target === event.curentTarget) {
-        $scope.showModal = false;
-      }
-    }; 
-    
+    $scope.open = function () {
+      var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: '../templates/modal.html',
+        controller: 'ModalInstanceCtrl',
+      });
+    };
+
   });
+
+  angular.module('bchatApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance, Rooms) {
+
+  //var newRoom = $scope.newRoom
+
+  $scope.ok = function () {
+    $modalInstance.dismiss('cancel');
+    $scope.rooms = Rooms.create('another room');
+     if(Rooms){
+
+     }
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
 
 
