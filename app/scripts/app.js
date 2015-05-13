d5376fb49b9cb59abab2cb85d7190a29b802a210
pcetx <moulthunl@gmail.com>
2015-05-13 16:04:00 +1000

    started adding the cookies

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,7 +21,21 @@ var app = angular.module('bchatApp', [
     'firebase.auth',
     'ui.bootstrap',
     'angular-loading-bar'
-]);
+])
+
+ .run(['$cookies', '$modal', function($cookies, $modal) {
+
+      if (!$cookies.blocChatCurrentUser || $cookies.blocChatCurrentUser === '' ) {
+
+        $modal.open({
+          templateUrl: '../templates/usermodal.html',
+          controller: 'UsernameCtrl',
+        })
+
+      }
+
+ }]);
+
 
 app.factory('Rooms', ['$firebaseArray','Ref', function($firebaseArray, Ref) {
 
@@ -29,13 +43,17 @@ app.factory('Rooms', ['$firebaseArray','Ref', function($firebaseArray, Ref) {
   var all = $firebaseArray(Ref.child('rooms'));
 
   //Add a new chatroom to the List according to input name
-  var create = function(name){
-    all.$add(name);
+  var create = function(newRoom){
+    all.$add(newRoom);
   };
 
   //Remove a room based on name
   var remove = function(name){
     all.$remove(name);
+  };
+
+  var messages = function(roomId) {
+      // Message query logic
   };
 
   return {
---
+++ app/scripts/controllers/username.js
@@ -0,0 +1,14 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name bchatApp.controller:LoginCtrl
+ * @description
+ * # UsernameCtrl
+ * Manages authentication to any active providers.
+ */
+angular.module('bchatApp')
+  .controller('UsernameCtrl', function ($scope) {
+ 
+   
+
+  });
